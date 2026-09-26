1c95c754b8dae040fdcbdfefb7241468c477681d
sudarshan-sh <mail2sudarshan.sharma@gmail.com>
2026-09-27 00:04:44 +0530

    feat: refactor theme provider import and implementation for improved structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist_Mono, Inter } from "next/font/google";
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 import "./globals.css";
 import { QueryProvider } from "@/components/providers/query-provider";
 import { ClerkProvider } from "@clerk/nextjs";
